45fe3e56252c8aac00ca31f732dc861f4e6f5d0d
JamieGavin1 <jamiegavin1@hotmail.com>
2016-10-03 16:59:51 +0100

    Power Rings go

---
+++ GameProject2016/Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Spawner : MonoBehaviour {
+
+	public GameObject SpeedRing;
+	public GameObject PlayerShip;
+	
+	void Start () {
+		InvokeRepeating ("SpawnSpeedRing", 5, 5);
+		SpawnSpeedRing();
+
+	}
+
+	void Update () {
+	
+	}
+
+	void SpawnSpeedRing()
+	{
+		Instantiate (SpeedRing, new Vector3 (Random.Range (-8, 8), Random.Range (-5, 5), (PlayerShip.transform.position.z + 250)), Quaternion.Euler (-90, 0, 0));
+		Debug.Log ("Speed ring spawned.");
+	}
+}
